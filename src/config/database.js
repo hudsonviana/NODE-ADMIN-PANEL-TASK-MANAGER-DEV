09fcf31e4e884d1c2768c1e7dd55518651fcfcff
hudsonviana <hudson1206@gmail.com>
2022-11-21 01:24:23 -0300

    configuraçaõ banco de dados com sequelize

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: process.env.DB_DIALECT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  define: {
+    timestamp: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
